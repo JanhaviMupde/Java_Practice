24fd47889c0401b76e34d6f8e78969efa6227697
Janhavi Mupde <mupdejanhavi@gmail.com>
2026-02-25 22:49:51 +0530

    Add files via upload

---
+++ JavaPractice/Ganesh.java
@@ -0,0 +1,9 @@
+//16th Sept 2025
+
+class Ganesh
+{
+    public static void main(String Arr[])
+    {
+        System.out.println("Jay Ganesh..");
+    }
+}
---
+++ JavaPractice/SequenceDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class SequenceDemo
+{
+    public static void main(String A[])
+    {
+        int iNo = 10;
+        int iNo2 = 11;
+        int iAns = 0;
+
+        iAns = iNo*iNo2;
+        System.out.println("Product is : "+iAns);
+
+    }
+}
